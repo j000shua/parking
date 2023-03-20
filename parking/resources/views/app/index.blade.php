660a5e122220c3718091e5b2085b7cfcab485e62
tdric <98964215+tdric@users.noreply.github.com>
2023-03-20 11:42:02 +0100

    différence entre page accueil admin et user classico

--- parking/resources/views/app/index.blade.php
+++ parking/resources/views/app/index.blade.php
@@ -9,7 +9,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    
+                    @if(Auth::user()->is_admin)
+                    <p>Tableau des réservations en cours</p>
+                    @else
                     <form method="post" action="{{ route('app.store') }}" class="mt-6 space-y-6">
                         @csrf
 
@@ -17,6 +19,7 @@
                             <x-primary-button>{{ __('Demander une place') }}</x-primary-button>
                         </div>
                     </form>
+                    @endif
                 </div>
             </div>
         </div>
